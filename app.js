4001a0fcb186cda1328deb2c975ac365aed3adcd
sicktooth <sosaronungwe@gmail.com>
2024-09-24 12:14:06 +0100

    complete challenge 18-19

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -55,15 +56,20 @@ app.post("/compose", function(req, res){
 })
 
 app.get("/posts/:postName", function(req, res){
-  let requestedTitle = req.params.postName
+  let requestedTitle = _.lowerCase(req.params.postName);
+  let requestedTitleInKabab = _.kebabCase(requestedTitle);
   posts.forEach(post => {
-    let storedTitle = post.title
+    let storedTitle = _.lowerCase(post.title);
+    let storedTitleInKabab = _.kebabCase(storedTitle);
 
-    if (requestedTitle === storedTitle) {
-      console.log("match found");
+    if (requestedTitleInKabab === storedTitleInKabab) {
+      // console.log("match found");
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      })
     } else {
       console.log("match not found");
-      
     }
   });
   
